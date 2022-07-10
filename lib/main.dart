cd39b5ba99451def4a3d6d18fb3e663c5f2d9b44
animousen4 <ilya_kozyro@mail.ru>
2022-07-10 23:09:26 +0300

    firebase impl

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,29 @@
+import 'dart:developer';
+
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_play_chess/app/app.dart';
+import 'package:flutter_play_chess/firebase_options.dart';
 import 'package:flutter_play_chess/service/user/user.dart';
 import 'package:flutter_play_chess/service/user/user_service.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:logger/logger.dart';
 
 void main() async {
+  final logger = Logger();
+
   await Hive.initFlutter();
   Hive.registerAdapter(UserAdapter());
 
   final userService = UserService();
   await userService.loadUser();
+
+  try {
+    await Firebase.initializeApp(
+        options: DefaultFirebaseOptions.currentPlatform);
+  } catch (e) {
+    logger.e("Error Firebase app initialization:\n$e");
+  }
 
   runApp(App(userService: userService));
 }
