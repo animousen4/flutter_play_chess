74930f80317728ef02846fd18079054c3cef7a8f
animousen4 <ilya_kozyro@mail.ru>
2022-08-23 22:09:14 +0300

    upd

--- lib/logic/client/networking_helper.dart
+++ lib/logic/client/networking_helper.dart
@@ -1,12 +1,28 @@
+import 'package:chopper/chopper.dart';
 import 'package:flutter_play_chess/logic/model/abstract_request.dart';
 import 'package:flutter_play_chess/logic/model/abstract_response.dart';
 
 class NetworkingHelper {
-  final Function request;
+  final Future<Response<AbstractResponse>> Function() request;
 
-  NetworkingHelper({required this.request});
+  final Function(Response<AbstractResponse> response) onSuccess;
+  final Function(Exception error) onError;
 
+  NetworkingHelper(
+      {required this.request, required this.onSuccess, required this.onError});
+
+  //https://betterprogramming.pub/functional-error-handling-in-flutter-ac6fcf8ae22b
   void makeRequest() async {
-    //var response = request();
+    try {
+      final response = await request();
+      checkCode(response.statusCode);
+      if (response.body != null) {
+        
+      }
+    } on Exception catch (e) {
+      onError(e);
+    }
   }
+
+  void checkCode(int code) {}
 }
